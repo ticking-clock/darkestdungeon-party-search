6807c15c3d4f07c52ed85eb505251f2961772236
Alan Kendall <alankendall@gmail.com>
2017-10-28 21:41:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Darkest Dungeon Party Search
+A dead-simple tool for finding named party combinations in the game Darkest Dungeon.
+
+https://ticking-clock.github.io/darkestdungeon-party-search/
+
+How to use
+---
+Choose the classes you want to include in the party combinations by selecting them in the drop-downs.
+You may enter the classes in any order.
---
+++ assets/main.js
@@ -0,0 +1,36 @@
+function initTable() {
+  $('.table-container').load('./assets/paste.html', function() {
+    // Tweak some of the pasted html from https://darkestdungeon.gamepedia.com/Party_Combos
+    $(this).find('table').css('width', 'auto');
+    $(this).find('table a img').unwrap().each(function(index, img) {
+      var $img = $(img);
+      var cls = $img.attr('alt').replace(' portrait roster.png', '').toLowerCase();
+      $img.attr('alt', cls).attr('src', 'assets/images/' + cls + '.png');
+    });
+  });
+}
+
+function initFilters() {
+  $('.filters select').on('change', function() {
+    updateRows();
+  });
+}
+
+function updateRows() {
+  var includeClasses = $('.filters select').map(function() {
+    var v = $(this).val();
+    if (v !== 'Select') return v.toLowerCase();
+  }).toArray();
+  $('.table-container tr').slice(1).each(function(index, row) {
+    console.log(row);
+    var rowClasses = $(row).find('img').map(function() { return $(this).attr('alt'); }).toArray();
+    $(row).toggle(includeClasses.every(function(cls) { return rowClasses.includes(cls); }));
+  });
+  var count = $('.table-container tr:visible').slice(1).length;
+  $('.filters .results').text(count + (count === 1 ? ' result' : ' results'));
+}
+
+$(function() {
+  initTable();
+  initFilters();
+});
